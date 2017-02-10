822e47ebe474fee138d25e662c079b1447a0d307
Patrick Hund <pahund@team.mobile.de>
2017-02-10 11:43:29 +0100

    fixed routing so that reload always works; linking file names to their source
    Change-Id: I9e62142924844bdd347285737d9e834b607154cd

--- src/server/index.js
+++ src/server/index.js
@@ -18,34 +18,32 @@ import sourceMapSupport from 'source-map-support';
 sourceMapSupport.install();
 
 const app = express();
-app.use(express.static('./dist'));
+app.use('/static', express.static('./dist'));
 
-app.get('/', (req, res) => res.send(`
-<!DOCTYPE html>
+app.get('*', (req, res) => res.send(`<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Universal React Router 4 Demo</title>
-    <style>
-        body {
-            font-family: Helvetica Neue, Arial, sans-serif;
-            margin: 0;
-        }
-        html { box-sizing: border-box; }
-        *, *:before, *:after { box-sizing: inherit; }
-    </style>
-</head>
-<body>
-    <div id="app">
-        ${renderToStaticMarkup(
-            <Router context={{}} location={req.url}>
-                <App />
-            </Router>
-        )}
-    </div>
-    <script src="client.js"></script>
-</body>
-</html>
-`));
+    <head>
+        <meta charset="UTF-8">
+        <title>Universal React Router 4 Demo</title>
+        <style>
+            body {
+                font-family: Helvetica Neue, Arial, sans-serif;
+                margin: 0;
+            }
+            html { box-sizing: border-box; }
+            *, *:before, *:after { box-sizing: inherit; }
+        </style>
+    </head>
+    <body>
+        <div id="app">
+            ${renderToStaticMarkup(
+                <Router context={{}} location={req.url}>
+                    <App />
+                </Router>
+            )}
+        </div>
+        <script src="/static/client.js"></script>
+    </body>
+</html>`));
 
 app.listen(3000, () => console.log('Demo app listening on port 3000'));
--- src/shared/Gist.js
+++ src/shared/Gist.js
@@ -14,7 +14,15 @@ export default ({ gist }) => (
         <p><a href={gist.html_url}>view on GitHub</a></p>
         <p>Files:</p>
         <ul>
-            { Object.keys(gist.files).map(key => <li key={key}>{key}</li>) }
+            {
+                Object.keys(gist.files).map(key => (
+                    <li key={key}>
+                        <a href={gist.files[key].raw_url}>
+                            {key}
+                        </a>
+                    </li>
+                ))
+            }
         </ul>
     </div>
 );
